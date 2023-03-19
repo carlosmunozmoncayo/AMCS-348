c456b0ebbd22184dba2c82b4bf3d417415ba52d6
carlosmunozmoncayo <carlos.munozmoncayo@kaust.edu.sa>
2023-03-19 14:15:15 +0300

    Mapped SBP operators working

--- SBP_matrices.py
+++ SBP_matrices.py
@@ -18,22 +18,14 @@ def first_order_D( x_nodes='LGL' , n=10 ):
 def first_order_P_Q( x_Lagrange_nodes='LGL', x_abcissae='LGL' , n=10 ):
     if x_Lagrange_nodes == 'LGL':
         _,_,_,_,_,_,x_Lagrange_nodes,_ = generate_LGL_points(n-1)
-    elif x_Lagrange_nodes == 'LG':
-        _,_,_,_,x_Lagrange_nodes,_,_,_ = generate_LGL_points(n-1)
 
     n_nodes = len(x_Lagrange_nodes)
     
-    
-    _,_,_,_,_,_,_, w_LGL = generate_LGL_points(n_nodes-1)
-    w_LGL = 0.5(x_abcissae[-1]-x_abcissae[0])*w_LGL
-
     if x_abcissae == 'LGL':
-        _,_,_,_,_,_,x_LGL, w_LGL = generate_LGL_points(n_nodes-1)
-    elif x_abcissae == 'LG':
-        _,_,_,_,x_LGL,w_LGL,_,_ = generate_LGL_points(n_nodes-1)
+        _,_,_,_,_,_,x_abcissae, w_abcissae = generate_LGL_points(n_nodes-1)
 
-    
-
+    _,_,_,_,_,_,_, w_abcissae = generate_LGL_points(n_nodes-1)
+    w_abcissae = 0.5*(x_abcissae[-1]-x_abcissae[0])*w_abcissae
 
     #Generating a list of Lagrange basis
     list_Lagrange_poly = []
@@ -46,17 +38,17 @@ def first_order_P_Q( x_Lagrange_nodes='LGL', x_abcissae='LGL' , n=10 ):
     #Filling matrix P
     P = np.zeros((n_nodes, n_nodes))
     for l in range(n_nodes):
-        L_vec = np.array([ Lagrange_poly(x_LGL[l]) for Lagrange_poly in list_Lagrange_poly])
-        P += w_LGL[l]*np.outer(L_vec,L_vec)
+        L_vec = np.array([ Lagrange_poly(x_abcissae[l]) for Lagrange_poly in list_Lagrange_poly])
+        P += w_abcissae[l]*np.outer(L_vec,L_vec)
 
     #Filling matrix Q (It's faster to build it as Q=PD if we have D)
     #This could be done in the previous loop, but in practice we wouldn't even 
     #compute Q like this
     Q = np.zeros((n_nodes,n_nodes))
     for l in range(n_nodes):
-        L_vec = np.array([ Lagrange_poly(x_LGL[l]) for Lagrange_poly in list_Lagrange_poly])
-        Lp_vec = np.array([ Lagrange_poly_prime(x_LGL[l]) for Lagrange_poly_prime in list_Lagrange_poly_prime])
-        Q += w_LGL[l]*np.outer(L_vec, Lp_vec)
+        L_vec = np.array([ Lagrange_poly(x_abcissae[l]) for Lagrange_poly in list_Lagrange_poly])
+        Lp_vec = np.array([ Lagrange_poly_prime(x_abcissae[l]) for Lagrange_poly_prime in list_Lagrange_poly_prime])
+        Q += w_abcissae[l]*np.outer(L_vec, Lp_vec)
 
     return P,Q
         
